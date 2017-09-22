be628872d66e5802ab2bda8193bea70ae14bc2cb
suganyar2 <rls.suganya@gmail.com>
2017-09-22 15:12:39 -0500

    CountryCode Repository - Implementing Add, Update, Delete, Get and Clear methods. Added Storage handler for handling the read, write, update to the file.

--- Interview-Exercise/CountryCodeRepository.cs
+++ Interview-Exercise/CountryCodeRepository.cs
@@ -1,7 +1,77 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
 namespace Interview_Exercise
 {
-    public class CountryCodeRepository
+    public interface ICountryCodeRepository
     {
+        void Add(Country country);
+        void Update(Country country);
+        void Delete(string countryCode);
+        Country Get(string countryCode);
+        void Clear();
+    }
+
+    public class CountryCodeRepository : ICountryCodeRepository
+    {
+        IStorageHandler _storageHandler = null;
+        Dictionary<string, string> _countriesList;
+        string noCountryExist = "Country does not exist.";
+        string countryExists = "Country already exists.";
+        string countryCodeRegx = "^[a-zA-Z]{3}$";
+        string invalidCountyCode = "Invalid Country Code.";
+
+        public CountryCodeRepository(IStorageHandler storageHandler)
+        {
+            _storageHandler = storageHandler;
+            _countriesList = _storageHandler.ReadAll();
+        }
+
+        public void Add(Country country)
+        {
+            Regex rx = new Regex(countryCodeRegx, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            if(!rx.IsMatch(country.Code))
+            {
+                throw new Exception(invalidCountyCode);
+            }
+            if (_countriesList.ContainsKey(country.Code.ToUpper()))
+            {
+                throw new Exception(countryExists);
+            }
+            _storageHandler.Write(country.Code.ToUpper(), country.Name);
+            _countriesList.Add(country.Code, country.Name);           
+        }
+
+        public void Clear()
+        {
+            _storageHandler.RemoveAll();
+        }
+
+        public void Delete(string countryCode)
+        {
+            countryCode = countryCode.ToUpper();
+            if (!_countriesList.ContainsKey(countryCode))
+                throw new Exception(noCountryExist);            
+            _countriesList.Remove(countryCode);
+            _storageHandler.Remove(countryCode);
+        }
+
+        public Country Get(string countryCode)
+        {
+            countryCode = countryCode.ToUpper();
+            if (!_countriesList.ContainsKey(countryCode))
+                throw new Exception(noCountryExist);
+            return new Country { Code = countryCode, Name = _countriesList[countryCode] };          
+        }
+
+        public void Update(Country country)
+        {
+            country.Code = country.Code.ToUpper();
+            if (!_countriesList.ContainsKey(country.Code))
+                throw new Exception(noCountryExist);
+            _storageHandler.Update(country.Code, country.Name);
+            _countriesList[country.Code] = country.Name;
+        }
     }
 }
---
+++ Interview-Exercise/StorageHandler.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Interview_Exercise
+{
+    public interface IStorageHandler
+    {
+        Dictionary<string, string> ReadAll();
+
+        void Write(string key, string value);
+
+        void Update(string key, string value);
+
+        void Remove(string key);
+
+        void RemoveAll();
+    }
+
+    public class CSVStorageHandler : IStorageHandler
+    {
+        string _storagePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "Interview-Exercise");
+
+        public Dictionary<string, string> ReadAll()
+        {
+            IEnumerable<string> files = Directory.EnumerateFiles(_storagePath).Where(file => file.EndsWith(".csv"));
+            var dict = new Dictionary<string, string>();
+            foreach (var file in files)
+            {
+                string[] allLines = File.ReadAllLines(file);
+                foreach (string line in allLines)
+                {
+                    string[] keyValue = line.Split(',');
+                    dict.Add(keyValue[0], keyValue[1]);
+                }
+            }
+            return dict;
+        }
+
+        public void Write(string key, string value)
+        {
+            string filePath = $@"{_storagePath}\{key[0]}.csv";
+            File.AppendAllText(filePath, $"{key},{value}\n");
+        }
+
+        public void Update(string key, string value)
+        {
+            string filePath = $@"{_storagePath}\{key[0]}.csv";
+            var lines = File.ReadAllLines(filePath).Where(line => !line.StartsWith(key));
+            File.WriteAllLines(filePath, lines);
+            File.AppendAllText(filePath, $"{key},{value}\n");
+        }
+
+        public void Remove(string key)
+        {
+            string filePath = $@"{_storagePath}\{key[0]}.csv";
+            var lines = File.ReadAllLines(filePath).Where(line => !line.StartsWith(key));
+            File.WriteAllLines(filePath, lines);
+        }
+
+        public void RemoveAll()
+        {
+            Array.ForEach(Directory.GetFiles(_storagePath), delegate (string path) { File.Delete(path); });
+        }
+    }
+}
